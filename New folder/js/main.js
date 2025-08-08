985bbcbcb60700fe200d3bd7ba2d6e482d6dc09e
NuriddinovBunyod <nuriddinov.bunyodbek@icloud.com>
2025-08-08 11:13:11 +0500

    first

---
+++ New folder/js/main.js
@@ -0,0 +1,6 @@
+// document.querySelector(".btn").onclick = function () {
+//     document.querySelector(".title").style.display = "none"
+// }
+firstName=prompt("ismini kirit")
+lastName=prompt("famlyani krt")
+document.querySelector(".ism-familya").innerHTML=firstName +" "+ lastName
